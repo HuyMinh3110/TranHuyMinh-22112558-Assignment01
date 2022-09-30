974f60fd022bc5463d9b1121b2751c1460001502
HuyMinh3110 <tranhuyminh3110@gmail.com>
2022-09-30 16:07:57 +0700

    Traffic Lights

--- TranHuyMinh-22112558-Assignment01/Form1.Designer.cs
+++ TranHuyMinh-22112558-Assignment01/Form1.Designer.cs
@@ -28,103 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnl_top = new System.Windows.Forms.Panel();
-            this.pnl_red = new System.Windows.Forms.Panel();
-            this.pnl_yellow = new System.Windows.Forms.Panel();
-            this.pnl_green = new System.Windows.Forms.Panel();
+            this.pnl_light_1 = new System.Windows.Forms.Panel();
+            this.pnl_red_1 = new System.Windows.Forms.Panel();
+            this.pnl_yellow_1 = new System.Windows.Forms.Panel();
+            this.pnl_green_1 = new System.Windows.Forms.Panel();
             this.btn_start = new System.Windows.Forms.Button();
             this.btn_stop = new System.Windows.Forms.Button();
             this.btn_reset = new System.Windows.Forms.Button();
-            this.pnl_top.SuspendLayout();
+            this.pnl_control = new System.Windows.Forms.Panel();
+            this.pnl_light_2 = new System.Windows.Forms.Panel();
+            this.pnl_red_2 = new System.Windows.Forms.Panel();
+            this.pnl_yellow_2 = new System.Windows.Forms.Panel();
+            this.pnl_green_2 = new System.Windows.Forms.Panel();
+            this.lbl_timer_1 = new System.Windows.Forms.Label();
+            this.pnl_timer_1 = new System.Windows.Forms.Panel();
+            this.pnl_timer_2 = new System.Windows.Forms.Panel();
+            this.lbl_timer_2 = new System.Windows.Forms.Label();
+            this.pnl_light_1.SuspendLayout();
+            this.pnl_control.SuspendLayout();
+            this.pnl_light_2.SuspendLayout();
+            this.pnl_timer_1.SuspendLayout();
+            this.pnl_timer_2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pnl_top
+            // pnl_light_1
             // 
-            this.pnl_top.BackColor = System.Drawing.Color.Black;
-            this.pnl_top.Controls.Add(this.pnl_green);
-            this.pnl_top.Controls.Add(this.pnl_yellow);
-            this.pnl_top.Controls.Add(this.pnl_red);
-            this.pnl_top.Location = new System.Drawing.Point(13, 13);
-            this.pnl_top.Name = "pnl_top";
-            this.pnl_top.Size = new System.Drawing.Size(239, 482);
-            this.pnl_top.TabIndex = 0;
+            this.pnl_light_1.BackColor = System.Drawing.Color.Black;
+            this.pnl_light_1.Controls.Add(this.pnl_timer_1);
+            this.pnl_light_1.Controls.Add(this.pnl_green_1);
+            this.pnl_light_1.Controls.Add(this.pnl_yellow_1);
+            this.pnl_light_1.Controls.Add(this.pnl_red_1);
+            this.pnl_light_1.Location = new System.Drawing.Point(14, 12);
+            this.pnl_light_1.Name = "pnl_light_1";
+            this.pnl_light_1.Size = new System.Drawing.Size(751, 138);
+            this.pnl_light_1.TabIndex = 0;
             // 
-            // pnl_red
+            // pnl_red_1
             // 
-            this.pnl_red.BackColor = System.Drawing.Color.Red;
-            this.pnl_red.Location = new System.Drawing.Point(42, 145);
-            this.pnl_red.Name = "pnl_red";
-            this.pnl_red.Size = new System.Drawing.Size(154, 84);
-            this.pnl_red.TabIndex = 0;
+            this.pnl_red_1.BackColor = System.Drawing.Color.Red;
+            this.pnl_red_1.Location = new System.Drawing.Point(29, 29);
+            this.pnl_red_1.Name = "pnl_red_1";
+            this.pnl_red_1.Size = new System.Drawing.Size(119, 84);
+            this.pnl_red_1.TabIndex = 0;
             // 
-            // pnl_yellow
+            // pnl_yellow_1
             // 
-            this.pnl_yellow.BackColor = System.Drawing.Color.Yellow;
-            this.pnl_yellow.Location = new System.Drawing.Point(42, 253);
-            this.pnl_yellow.Name = "pnl_yellow";
-            this.pnl_yellow.Size = new System.Drawing.Size(154, 84);
-            this.pnl_yellow.TabIndex = 1;
+            this.pnl_yellow_1.BackColor = System.Drawing.Color.Yellow;
+            this.pnl_yellow_1.Location = new System.Drawing.Point(189, 29);
+            this.pnl_yellow_1.Name = "pnl_yellow_1";
+            this.pnl_yellow_1.Size = new System.Drawing.Size(119, 84);
+            this.pnl_yellow_1.TabIndex = 1;
             // 
-            // pnl_green
+            // pnl_green_1
             // 
-            this.pnl_green.BackColor = System.Drawing.Color.Lime;
-            this.pnl_green.Location = new System.Drawing.Point(42, 367);
-            this.pnl_green.Name = "pnl_green";
-            this.pnl_green.Size = new System.Drawing.Size(154, 84);
-            this.pnl_green.TabIndex = 2;
+            this.pnl_green_1.BackColor = System.Drawing.Color.Lime;
+            this.pnl_green_1.Location = new System.Drawing.Point(359, 29);
+            this.pnl_green_1.Name = "pnl_green_1";
+            this.pnl_green_1.Size = new System.Drawing.Size(119, 84);
+            this.pnl_green_1.TabIndex = 2;
             // 
             // btn_start
             // 
-            this.btn_start.Location = new System.Drawing.Point(414, 278);
+            this.btn_start.Location = new System.Drawing.Point(48, 40);
             this.btn_start.Name = "btn_start";
             this.btn_start.Size = new System.Drawing.Size(83, 54);
             this.btn_start.TabIndex = 1;
             this.btn_start.Text = "Start";
             this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
             // 
             // btn_stop
             // 
-            this.btn_stop.Location = new System.Drawing.Point(526, 278);
+            this.btn_stop.Location = new System.Drawing.Point(196, 40);
             this.btn_stop.Name = "btn_stop";
             this.btn_stop.Size = new System.Drawing.Size(83, 54);
             this.btn_stop.TabIndex = 2;
             this.btn_stop.Text = "Stop";
             this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
             // 
             // btn_reset
             // 
-            this.btn_reset.Location = new System.Drawing.Point(639, 278);
+            this.btn_reset.Location = new System.Drawing.Point(342, 40);
             this.btn_reset.Name = "btn_reset";
             this.btn_reset.Size = new System.Drawing.Size(85, 54);
             this.btn_reset.TabIndex = 3;
             this.btn_reset.Text = "Reset";
             this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // pnl_control
+            // 
+            this.pnl_control.BackColor = System.Drawing.Color.Cyan;
+            this.pnl_control.Controls.Add(this.btn_start);
+            this.pnl_control.Controls.Add(this.btn_reset);
+            this.pnl_control.Controls.Add(this.btn_stop);
+            this.pnl_control.Location = new System.Drawing.Point(725, 370);
+            this.pnl_control.Name = "pnl_control";
+            this.pnl_control.Size = new System.Drawing.Size(458, 125);
+            this.pnl_control.TabIndex = 4;
+            // 
+            // pnl_light_2
+            // 
+            this.pnl_light_2.BackColor = System.Drawing.Color.Black;
+            this.pnl_light_2.Controls.Add(this.pnl_timer_2);
+            this.pnl_light_2.Controls.Add(this.pnl_green_2);
+            this.pnl_light_2.Controls.Add(this.pnl_yellow_2);
+            this.pnl_light_2.Controls.Add(this.pnl_red_2);
+            this.pnl_light_2.Location = new System.Drawing.Point(14, 201);
+            this.pnl_light_2.Name = "pnl_light_2";
+            this.pnl_light_2.Size = new System.Drawing.Size(751, 144);
+            this.pnl_light_2.TabIndex = 5;
+            // 
+            // pnl_red_2
+            // 
+            this.pnl_red_2.BackColor = System.Drawing.Color.Red;
+            this.pnl_red_2.Location = new System.Drawing.Point(29, 35);
+            this.pnl_red_2.Name = "pnl_red_2";
+            this.pnl_red_2.Size = new System.Drawing.Size(119, 80);
+            this.pnl_red_2.TabIndex = 0;
+            // 
+            // pnl_yellow_2
+            // 
+            this.pnl_yellow_2.BackColor = System.Drawing.Color.Yellow;
+            this.pnl_yellow_2.Location = new System.Drawing.Point(189, 35);
+            this.pnl_yellow_2.Name = "pnl_yellow_2";
+            this.pnl_yellow_2.Size = new System.Drawing.Size(119, 80);
+            this.pnl_yellow_2.TabIndex = 1;
+            // 
+            // pnl_green_2
+            // 
+            this.pnl_green_2.BackColor = System.Drawing.Color.Lime;
+            this.pnl_green_2.Location = new System.Drawing.Point(359, 35);
+            this.pnl_green_2.Name = "pnl_green_2";
+            this.pnl_green_2.Size = new System.Drawing.Size(119, 80);
+            this.pnl_green_2.TabIndex = 2;
+            // 
+            // lbl_timer_1
+            // 
+            this.lbl_timer_1.AutoSize = true;
+            this.lbl_timer_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_timer_1.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_timer_1.Location = new System.Drawing.Point(27, 18);
+            this.lbl_timer_1.Name = "lbl_timer_1";
+            this.lbl_timer_1.Size = new System.Drawing.Size(66, 48);
+            this.lbl_timer_1.TabIndex = 3;
+            this.lbl_timer_1.Text = "36";
+            this.lbl_timer_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pnl_timer_1
+            // 
+            this.pnl_timer_1.BackColor = System.Drawing.Color.Silver;
+            this.pnl_timer_1.Controls.Add(this.lbl_timer_1);
+            this.pnl_timer_1.Location = new System.Drawing.Point(573, 29);
+            this.pnl_timer_1.Name = "pnl_timer_1";
+            this.pnl_timer_1.Size = new System.Drawing.Size(116, 84);
+            this.pnl_timer_1.TabIndex = 4;
+            // 
+            // pnl_timer_2
+            // 
+            this.pnl_timer_2.BackColor = System.Drawing.Color.Silver;
+            this.pnl_timer_2.Controls.Add(this.lbl_timer_2);
+            this.pnl_timer_2.Location = new System.Drawing.Point(573, 35);
+            this.pnl_timer_2.Name = "pnl_timer_2";
+            this.pnl_timer_2.Size = new System.Drawing.Size(116, 80);
+            this.pnl_timer_2.TabIndex = 3;
+            // 
+            // lbl_timer_2
+            // 
+            this.lbl_timer_2.AutoSize = true;
+            this.lbl_timer_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_timer_2.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_timer_2.Location = new System.Drawing.Point(27, 17);
+            this.lbl_timer_2.Name = "lbl_timer_2";
+            this.lbl_timer_2.Size = new System.Drawing.Size(66, 48);
+            this.lbl_timer_2.TabIndex = 0;
+            this.lbl_timer_2.Text = "12";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1195, 507);
-            this.Controls.Add(this.btn_reset);
-            this.Controls.Add(this.btn_stop);
-            this.Controls.Add(this.btn_start);
-            this.Controls.Add(this.pnl_top);
+            this.Controls.Add(this.pnl_light_2);
+            this.Controls.Add(this.pnl_control);
+            this.Controls.Add(this.pnl_light_1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.pnl_top.ResumeLayout(false);
+            this.pnl_light_1.ResumeLayout(false);
+            this.pnl_control.ResumeLayout(false);
+            this.pnl_light_2.ResumeLayout(false);
+            this.pnl_timer_1.ResumeLayout(false);
+            this.pnl_timer_1.PerformLayout();
+            this.pnl_timer_2.ResumeLayout(false);
+            this.pnl_timer_2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pnl_top;
-        private System.Windows.Forms.Panel pnl_green;
-        private System.Windows.Forms.Panel pnl_yellow;
-        private System.Windows.Forms.Panel pnl_red;
+        private System.Windows.Forms.Panel pnl_light_1;
+        private System.Windows.Forms.Panel pnl_green_1;
+        private System.Windows.Forms.Panel pnl_yellow_1;
+        private System.Windows.Forms.Panel pnl_red_1;
         private System.Windows.Forms.Button btn_start;
         private System.Windows.Forms.Button btn_stop;
         private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Panel pnl_control;
+        private System.Windows.Forms.Panel pnl_light_2;
+        private System.Windows.Forms.Panel pnl_green_2;
+        private System.Windows.Forms.Panel pnl_yellow_2;
+        private System.Windows.Forms.Panel pnl_red_2;
+        private System.Windows.Forms.Panel pnl_timer_1;
+        private System.Windows.Forms.Label lbl_timer_1;
+        private System.Windows.Forms.Panel pnl_timer_2;
+        private System.Windows.Forms.Label lbl_timer_2;
     }
 }
 
--- TranHuyMinh-22112558-Assignment01/Form1.cs
+++ TranHuyMinh-22112558-Assignment01/Form1.cs
@@ -16,5 +16,25 @@ namespace TranHuyMinh_22112558_Assignment01
         {
             InitializeComponent();
         }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
